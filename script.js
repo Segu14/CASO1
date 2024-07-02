bca0c1f7fdca4ba9d2c23fae1c5242b8d8664284
Sebastián Segura <ssegura30126@ufide.ac.cr>
2024-07-01 20:40:12 -0600

    100 Funcional

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const form = document.getElementById('serviciosPrestados');
+const formServ = document.getElementById('serviciosPrestados');
 const resultDiv = document.getElementById('result');
 
-form.addEventListener('submit', function(event) {
+formServ.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const checkboxes = form.querySelectorAll('input[type="checkbox"]:checked');
+    const checkboxes = formServ.querySelectorAll('input[type="checkbox"]:checked');
 
     let result = 0
     checkboxes.forEach(checkbox => {
@@ -15,4 +15,30 @@ form.addEventListener('submit', function(event) {
         El total a pagar es $${result}</p>
     `;
     
+});
+
+const formCita = document.getElementById('agendarCita');
+const resultDivCita = document.getElementById('resultCita');
+const citas = [];
+
+formCita.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const mascota = document.getElementById('nomMascota').value;
+    const duenho = document.getElementById('nomDuenho').value;
+    const fecha = document.getElementById('fecha').value;
+    const hora = document.getElementById('hora').value;
+
+    const cita = `
+        <p>Mascota: ${mascota}</p>
+        <p>Dueñ@: ${duenho}</p>
+        <p>Fecha Cita: ${fecha}</p>
+        <p>Hora Cita: ${hora}</p>
+        <br>
+    `;
+    citas.push(cita);
+
+    resultDivCita.innerHTML = citas.join('');
+
+    formCita.reset();
 });
